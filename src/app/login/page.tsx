0c00f59a583f08fe4cc47e6c4c2556de5ad210d6
Nacho114 <17376073+Nacho114@users.noreply.github.com>
2025-01-15 15:43:45 +0100

    WIP

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,12 +1,63 @@
 'use client'
 
-import { GalleryVerticalEnd } from 'lucide-react'
-
-import { LoginForm } from './login-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useForm } from 'react-hook-form'
+import { Button } from '@/components/ui/button'
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+import { LoginSchema, loginSchema } from '@/lib/supabase/auth'
+import { signIn } from './actions'
+import { useRouter } from 'next/navigation'
+import { useState, useTransition } from 'react'
+import Link from 'next/link'
+import { Loader, GalleryVerticalEnd } from 'lucide-react'
 
 export default function LoginPage() {
+  const router = useRouter()
+  const [errorMessage, setErrorMessage] = useState<string | null>(null)
+  const [isPending, startTransition] = useTransition()
+
+  const form = useForm<LoginSchema>({
+    resolver: zodResolver(loginSchema),
+    defaultValues: {
+      email: '',
+      password: '',
+    },
+  })
+
+  async function onSubmit(values: LoginSchema) {
+    startTransition(async () => {
+      setErrorMessage(null)
+
+      const response = await signIn(values)
+
+      if (response.success) {
+        router.push('/dashboard')
+      } else {
+        setErrorMessage(response.message)
+      }
+    })
+  }
+
+  if (isPending) {
+    return (
+      <div className="flex items-center justify-center h-screen w-screen bg-gray-100">
+        <div className="animate-spin">
+          <Loader size={48} />
+        </div>
+      </div>
+    )
+  }
+
   return (
-    <div className="flex min-h-svh flex-col items-center justify-center gap-6 bg-muted p-6 md:p-10">
+    <div className="flex min-h-screen flex-col items-center justify-center gap-6 bg-muted p-6 md:p-10">
       <div className="flex w-full max-w-sm flex-col gap-6">
         <a href="#" className="flex items-center gap-2 self-center font-medium">
           <div className="flex h-6 w-6 items-center justify-center rounded-md bg-primary text-primary-foreground">
@@ -14,7 +65,72 @@ export default function LoginPage() {
           </div>
           Atrinum
         </a>
-        <LoginForm />
+        <Card>
+          <CardHeader className="text-center">
+            <CardTitle className="text-xl">Welcome back</CardTitle>
+            <CardDescription>
+              Login with your email and password
+            </CardDescription>
+          </CardHeader>
+          <CardContent>
+            <form onSubmit={form.handleSubmit(onSubmit)}>
+              <div className="grid gap-6">
+                <div className="grid gap-2">
+                  <Label htmlFor="email">Email</Label>
+                  <Input
+                    id="email"
+                    type="email"
+                    placeholder="m@example.com"
+                    {...form.register('email')}
+                  />
+                  {form.formState.errors.email && (
+                    <p className="text-sm text-red-500">
+                      {form.formState.errors.email.message}
+                    </p>
+                  )}
+                </div>
+                <div className="grid gap-2">
+                  <div className="flex items-center">
+                    <Label htmlFor="password">Password</Label>
+                    <Link
+                      href="/auth/forgot-password"
+                      className="ml-auto text-sm underline-offset-4 hover:underline"
+                    >
+                      Forgot your password?
+                    </Link>
+                  </div>
+                  <Input
+                    id="password"
+                    type="password"
+                    placeholder="Enter your password"
+                    {...form.register('password')}
+                  />
+                  {form.formState.errors.password && (
+                    <p className="text-sm text-red-500">
+                      {form.formState.errors.password.message}
+                    </p>
+                  )}
+                </div>
+                <Button type="submit" className="w-full">
+                  Login
+                </Button>
+                {errorMessage && (
+                  <p className="mt-2 text-sm text-red-500">{errorMessage}</p>
+                )}
+              </div>
+            </form>
+          </CardContent>
+        </Card>
+        <div className="text-center text-sm mt-4">
+          Don&apos;t have an account?{' '}
+          <Link href="/auth/signup" className="underline underline-offset-4">
+            Sign up
+          </Link>
+        </div>
+        <div className="text-balance text-center text-xs text-muted-foreground mt-2 [&_a]:underline [&_a]:underline-offset-4 [&_a]:hover:text-primary">
+          By clicking continue, you agree to our{' '}
+          <a href="#">Terms of Service</a> and <a href="#">Privacy Policy</a>.
+        </div>
       </div>
     </div>
   )
