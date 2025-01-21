fce9f0a3b46e31aee776b2fd4f25f24edd3dffc7
Nacho114 <17376073+Nacho114@users.noreply.github.com>
2025-01-21 12:11:22 +0100

    Admin: home panel

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,5 +1,39 @@
-'use server'
+'use client'
 
-export default async function Admin() {
-  return <div>Admin</div>
+import { useRouter } from 'next/navigation'
+import { Card, CardHeader, CardTitle } from '@/components/ui/card'
+
+export default function Admin() {
+  const router = useRouter()
+
+  const sections = [
+    {
+      title: 'Manage Users',
+      path: '/admin/manage-users',
+    },
+    {
+      title: 'Pending Payments',
+      path: '/admin/pending-payments',
+    },
+    {
+      title: 'Pending Moves',
+      path: '/admin/pending-moves',
+    },
+  ]
+
+  return (
+    <div className="grid gap-6 max-w-xl mx-auto py-8">
+      {sections.map((section) => (
+        <Card
+          key={section.path}
+          onClick={() => router.push(section.path)}
+          className="hover:shadow-lg hover:cursor-pointer transition-shadow p-4"
+        >
+          <CardHeader>
+            <CardTitle className="text-center">{section.title}</CardTitle>
+          </CardHeader>
+        </Card>
+      ))}
+    </div>
+  )
 }
